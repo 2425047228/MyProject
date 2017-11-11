96bf07456bfab1ab90ba5c93e1ff00465af97372
yangshuaiWithCompany <go.xlv@qq.com>
2017-11-11 13:39:01 +0800

    新增logo

--- src/main.js
+++ src/main.js
@@ -1,58 +1,54 @@
-import React, {Component} from 'react';
-import ReactDOM from 'react-dom';
-import './main.css';
-import { Layout, Menu, Breadcrumb, Icon } from 'antd';
-const { Header, Content, Footer, Sider } = Layout;
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import './main.css'
+import { Layout, Menu, Breadcrumb, Icon } from 'antd'
+const { Header, Content, Footer, Sider } = Layout
 const SubMenu = Menu.SubMenu;
 
-class Main extends Component{
-    constructor(props) {
-        super(props);
-        this.state = {collapsed: false};
+class Main extends Component {
+    constructor (props) {
+        super(props)
+        this.state = {collapsed: false}
         this.toggle = this.toggle.bind(this);    //侧边栏样式切换
+        this.handleClick = this.handleClick.bind(this);
     }
 
-      toggle() {this.setState({collapsed: !this.state.collapsed});}
+    toggle () {this.setState({collapsed: !this.state.collapsed});}
+    handleClick(item) {
+        console.log(item);
+    }
 
-      render() {
+    render () {
+        let collapsed = this.state.collapsed;
         return (
-          <Layout style={{ minHeight: '100vh' }}>
-            <Sider
-              trigger={null}
-              collapsible
-              collapsed={this.state.collapsed}
-            >
-              <div className="logo" />
-              <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-                <Menu.Item key="1">
-                  <Icon type="user" />
-                  <span>nav 1</span>
-                </Menu.Item>
-                <Menu.Item key="2">
-                  <Icon type="video-camera" />
-                  <span>nav 2</span>
-                </Menu.Item>
-                <Menu.Item key="3">
-                  <Icon type="upload" />
-                  <span>nav 3</span>
-                </Menu.Item>
-              </Menu>
-            </Sider>
-            <Layout>
-              <Header style={{ background: '#fff', padding: 0 }}>
-                <Icon
-                  className="trigger"
-                  type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
-                  onClick={this.toggle}
-                />
-              </Header>
-              <Content style={{ margin: '24px 16px', padding: 24, background: '#fff', minHeight: 280 }}>
-                Content
-              </Content>
+            <Layout style={{height: '100vh'}}>
+                <Header className={'main-header' + (collapsed ? ' main-shrink' : '')}>
+                    <Icon className='main-trigger' type={collapsed ? 'menu-unfold' : 'menu-fold'} onClick={this.toggle}/>
+                </Header>
+                <Sider className='main-sider' trigger={null} collapsible collapsed={collapsed}>
+                    <div className='main-logo'><Icon type='windows'/></div>
+                    <Menu mode='inline' defaultSelectedKeys={['1']} onClick={this.handleClick}>
+                        <Menu.Item key='1'>
+                            <Icon type='user' /><span>nav 1</span>
+                        </Menu.Item>
+                        <Menu.Item key='2'>
+                            <Icon type='video-camera' /><span>nav 2</span>
+                        </Menu.Item>
+                        <Menu.Item key='3'>
+                            <Icon type='upload' /><span>nav 3</span>
+                        </Menu.Item>
+                        <SubMenu key="sub1" title={<span><Icon type="user"/><span>sub1</span></span>}>
+                            <Menu.Item key="4">option1</Menu.Item>
+                            <Menu.Item key="5">option2</Menu.Item>
+                            <Menu.Item key="6">option3</Menu.Item>
+                            <Menu.Item key="7">option4</Menu.Item>
+                        </SubMenu>
+                    </Menu>
+                </Sider>
+                <div className={'main-content' + (collapsed ? ' main-shrink' : '')}>dfdf</div>
             </Layout>
-          </Layout>
         );
-      }
+    }
 }
 
-ReactDOM.render(<Main/>,document.getElementById('root'));
+ReactDOM.render(<Main/>, document.getElementById('root'))
